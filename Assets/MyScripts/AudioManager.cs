d34152e73a9482559eae9e414e998c627cf99ab5
55thOperator <alittleflame01@gmail.com>
2026-03-03 11:07:49 +0700

    AddedBGM

---
+++ Assets/MyScripts/AudioManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public AudioSource musicSource;
+    public AudioSource ambientSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+    public void PlayAmbient(AudioClip clip)
+    {
+        ambientSource.clip = clip;
+        ambientSource.loop = true;
+        ambientSource.Play();
+    }
+}
